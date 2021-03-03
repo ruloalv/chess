d9478035527e120ceee3fed9d77dbfe15aeca4a0
ruloalv <ruloalv@gmail.com>
2021-03-03 16:00:10 -0300

    begginig board initialize

--- lib/board.rb
+++ lib/board.rb
@@ -1,3 +1,10 @@
 class Board
-	
+	attr_reader :grid, :whites, :blacks, :turn
+
+	def initialize(grid = Array.new(8) {Array.new(8)}, params = {})
+		@grid = grid
+		@whites = params[:whites]
+		@blacks = params[:blacks]
+		@trun = params[:turn]
+	end
 end
