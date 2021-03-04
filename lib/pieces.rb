83138f2fa91fa354e08b413fb1a841345c643e02
ruloalv <ruloalv@gmail.com>
2021-03-04 14:20:46 -0300

    create king class with posible_moves method

--- lib/pieces.rb
+++ lib/pieces.rb
@@ -1,3 +1,18 @@
-class piece
-	
+class Piece
+	attr_reader :name, :color, :position
+
+	def initialize(name, color, position)
+		@name = name
+		@color = color
+		@position = position
+	end
+end
+
+class King < Piece
+	@@MOVES = [[1,0],[1,1],[0,1],[-1,1],[-1,0],[-1,-1],[0,-1],[1,-1]]
+
+	def posible_moves
+		@@MOVES.map {|m| [@position[0] + m[0], @position[1] + m[1]}
+			   .keep_if {|m| valid_position(m)}
+	end
 end
