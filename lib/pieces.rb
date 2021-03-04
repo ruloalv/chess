62479d40dd88d580cd0166b1e674bfd781bf2f2a
ruloalv <ruloalv@gmail.com>
2021-03-04 15:23:59 -0300

    add initialize to King class

--- lib/pieces.rb
+++ lib/pieces.rb
@@ -18,6 +18,10 @@ end
 class King < Piece
 	@@MOVES = [[1,0],[1,1],[0,1],[-1,1],[-1,0],[-1,-1],[0,-1],[1,-1]]
 
+	def initialize(color, position)
+		super("King", color, position, " \u265A ")
+	end
+
 	def posible_moves
 		@@MOVES.map {|m| [@position[0] + m[0], @position[1] + m[1]}
 			   .keep_if {|m| valid_position(m)}
